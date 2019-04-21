d8e711508e1be0ff87192f0291a25df0150ae97d
Shawn Ren <shawnren527@gmail.com>
2019-04-21 20:11:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,14 +16,14 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
 
-    elsif req.path.match(/add/)
-      add_item = req.params["item"]
-      if @@items.include?(add_item)
-        @@cart << add_item
-        resp.write "added #{add_item}"
-      else
-        resp.write "We don't have that item"
-      end
+    # elsif req.path.match(/add/)
+    #   add_item = req.params["item"]
+    #   if @@items.include?(add_item)
+    #     @@cart << add_item
+    #     resp.write "added #{add_item}"
+    #   else
+    #     resp.write "We don't have that item"
+    #   end
 
     elsif req.path.match(/cart/)
       if @@cart = []
