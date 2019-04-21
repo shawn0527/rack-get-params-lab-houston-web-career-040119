4dab54db94bbdb7d03830af177791814a9043ba9
Shawn Ren <shawnren527@gmail.com>
2019-04-21 20:12:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,7 +27,7 @@ class Application
 
   elsif req.path.match(/add/)
     item_to_add = req.params["item"]
-    if @@items.include? item_to_add 
+    if @@items.include? item_to_add
       @@cart << item_to_add
       resp.write "added #{item_to_add}"
     else
